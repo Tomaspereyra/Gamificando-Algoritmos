ece444e359a8f197ed5bc5ec20b598b4319ffe4f
Luchoooo98 <31218127+Luchoooo98@users.noreply.github.com>
2018-10-19 15:59:45 -0300

    Actualizacion mazeEditor

--- Procesos Orientados al Desarrollo del Software/Proceso de implementación/Javascript/MazeEditor/controladorEditorMapa.js
+++ Procesos Orientados al Desarrollo del Software/Proceso de implementación/Javascript/MazeEditor/controladorEditorMapa.js
@@ -3,8 +3,8 @@ var gameScene = new Phaser.Scene('MazeScene');
 
 var config = {
 	type: Phaser.AUTO,
-	width: 512,
-	height: 512,
+	width: 575,
+	height: 575,
 	parent: 'phaser-div', //se define el canvas donde va a dibujar
 	scene: gameScene
 };
@@ -39,9 +39,9 @@ function mapIndexToSprite(index){
 }
 
 gameScene.create = function () {    
- var mapaPrueba=new Mapa(32,32);
+ var mapaPrueba=new Mapa(16,16);
  mapaPrueba.LlenarGrilla(PISO);
- dibujarMapa(mapaPrueba);	
+ dibujarTodo(mapaPrueba);	
 }
 
 
@@ -49,43 +49,101 @@ gameScene.update = function() {
 
 }
 
+var pincel=PISO;
 function dibujarMapa(mapa){
 	var sprite;
 	for (var row = 0; row < mapa.ancho; row++){
 		for (var col = 0; col < mapa.altura; col++){
 			//console.log("Drawing : Row,col = [" + row + "," + col + "] -> " + this.tiles[row][col]);
-			sprite = gameScene.add.sprite((SPRITE_SIZE *row),(SPRITE_SIZE * col), mapIndexToSprite(mapa.grilla[row][col])).setInteractive();
+			sprite = gameScene.add.sprite((SPRITE_SIZE *(row+1)),(SPRITE_SIZE * col), mapIndexToSprite(mapa.grilla[row][col])).setInteractive();
 			sprite.row = row
 			sprite.col = col
 
-			sprite.on('pointerdown', function (pointer) {
 
-        		this.setTint(0xff0000,console.log("X=",row ,"Y=",col));
-
-    		});
-
-    		sprite.on('pointermove', function (pointer) {
-
-       			x = pointer.x;
+			sprite.on('pointerdown', function (pointer) {//cuando tengo click
+				x = pointer.x;
         		y = pointer.y;
         		row=Math.floor((x/SPRITE_SIZE));  
         		col=Math.floor((y/SPRITE_SIZE));
+        		console.log("X=",row ,"Y=",col);
 
-   			 });
+                if(pincel==PARED){
+                    mapa.CambiarAPared(row,col);
+                    this.setTexture('Wall');
+                }
+                
+                if(pincel==PISO){
+                    mapa.CambiarAPiso(row,col);
+                    this.setTexture('Floor');
+                }
+                
+                if(pincel==INICIO){
+                    mapa.EstablecerInicio(row,col);
+                    this.setTexture('Start');
+                }
 
-   			 sprite.on('pointerout', function (pointer) {
+                if(pincel==SALIDA){
+                    mapa.EstablecerSalida(row,col);
+                    this.setTexture('Exit');
+                }
 
-        		this.clearTint(console.log("X=",row ,"Y=",col));
-
-   			 });
-
-   			 sprite.on('pointerup', function (pointer) {
-
-      		  this.clearTint(console.log("X=",row ,"Y=",col));
+        		this.setTint(0xff0000);//lo pinto de rojo
 
     		});
-			sprite.setOrigin(0, 0);
+
+
+   			 sprite.on('pointerup', function (pointer) {//cuando levanta el click del mouse lo vuelve a pintar gris
+
+      		  this.clearTint();//limpia el tint rojo a original
+
+    		});
+			sprite.setOrigin(0,0);
 		}
 	}
 }
 
+function paletaMapa(){
+			
+		for (var col = 0; col < 4; col++){
+			sprite = gameScene.add.sprite((SPRITE_SIZE *0),(SPRITE_SIZE * col), mapIndexToSprite(col)).setInteractive();
+			sprite.setOrigin(0,0);
+            sprite.col = col
+
+
+          
+            sprite.on('pointerdown', function (pointer) {//cuando tengo click
+                if (sprite.col=0) {
+                    pincel=PISO;
+                }
+                 if (sprite.col=1) {
+                    pincel=PARED;
+                }
+                 if (sprite.col=2) {
+                    pincel=INICIO;
+                }
+                 if (sprite.col=3) {
+                    pincel=SALIDA;
+                }
+
+                this.setTint(0xff0000);//lo pinto de rojo
+
+            });
+            
+            sprite.on('pointerup', function (pointer) {//cuando levanta el click del mouse lo vuelve a pintar gris
+
+              this.clearTint();//limpia el tint rojo a original
+
+            });   
+
+        }
+		
+	
+	//return this.AsignarTileSeleccionado(tileSeleccionado);
+
+}
+
+function dibujarTodo(mapa){
+	dibujarMapa(mapa);
+	paletaMapa();
+
+}
